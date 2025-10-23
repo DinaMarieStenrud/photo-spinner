793b1a1161e31fdac0b30a7d49f4381f70d25b21
Dina Marie Stenrud <dinamst@ntnu.no>
2025-10-23 13:10:52 +0200

    build: publish docs for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StartPage from "./StartPage";
-import FirstGameStarts from "./FirstGameStarts";
-import "./index.css";
+// main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import StartPage from './StartPage'
+import FirstGameStarts from './FirstGameStarts'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/fg/firstgamestarts" element={<FirstGameStarts />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-);
+)
+
